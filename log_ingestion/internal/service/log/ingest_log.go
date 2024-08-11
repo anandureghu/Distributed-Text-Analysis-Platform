3632e00d3c414b415c6a0aa995a957fbec3ac1da
anandureghu <anandukr145@gmail.com>
2024-08-11 23:59:02 +0530

    sepration of concerns initial

---
+++ log_ingestion/internal/service/log/ingest_log.go
@@ -0,0 +1,8 @@
+package log
+
+// Function to read and ingest a new log file
+func IngestLogFile(filePath string) error {
+	// TODO: Read the log file in chunks using pointers to manage memory efficiently
+	// and send the data to the Log Processing Service via an HTTP POST request.
+	return nil
+}
